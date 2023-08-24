7fb8ef9a79ce10a90ba1328a7220e008486a61ed
jlegario8 <61211849+jlegario8@users.noreply.github.com>
2023-08-24 09:44:42 -0300

    Update validations.py
    I fix the word return

--- validations.py
+++ validations.py
@@ -9,7 +9,7 @@ def validate_user(username, minlen):
         raise ValueError("minlen must at least 1")
         
     if len(username) < minlen:
-        ereturn False
+        return False
     if not username.isalnum():
         return False
     # Usernames can't begin with a number
